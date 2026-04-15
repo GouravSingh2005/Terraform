197f94505dd3a5ee5d1b9faa66cd40c975b20b88
Gourav-Singh-Comprinno <gourav.singh@comprinno.net>
2026-04-15 12:27:54 +0530

    Added buildspec.yml

--- backend/config/s3.js
+++ backend/config/s3.js
@@ -1,7 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.local" });
+dotenv.config({ path: ".env.prod" });
 
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
